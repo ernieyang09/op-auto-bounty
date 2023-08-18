58a29c07b0caef4244bdf0c0798fa65d801b31d5
Ernie Yang <ernieyang09@hotmail.com>
2023-08-18 18:34:06 +0800

    fix

--- cron.js
+++ cron.js
@@ -1,4 +1,4 @@
-// const BigNumber = require('bignumber.js')
+const yargs = require("yargs");
 const ethers = require("ethers")
 const optimismSDK = require("@eth-optimism/sdk")
 const { quantile } = require('simple-statistics')
@@ -6,7 +6,10 @@ const createContract = require('./src/contracts')
 const fetchPrice = require('./src/price')
 require('dotenv').config()
 
+const { argv } = yargs
+
 const { ALCHEMY_API_KEY, WALLET_PRIVATE_KEY, VAULT_CONTRACT_ADDR, TO_ADDRESS, MIN_REWARD_USD  } = process.env
+
 const minReward = parseFloat(MIN_REWARD_USD)
 const alchemyEndpoint = `https://opt-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`
 
@@ -33,21 +36,23 @@ const calculateGasFeeUsd = async (wallet, tx) => {
     const pWeiValue = samplePWei(hist.reward)
     const bWeiValue = ethers.BigNumber.from(hist.baseFeePerGas[hist.baseFeePerGas.length-1])
     const gasPrice = bWeiValue.add(pWeiValue)
-    return [gasPrice, ethers.BigNumber.from(15).mul(l2gas).mul(gasPrice)]
+
+    console.log('l2gas gwei', ethers.utils.formatUnits(gasPrice, 'gwei'))
+
+    return [pWeiValue, ethers.BigNumber.from(15).div(10).mul(l2gas).mul(gasPrice)]
   }
 
   const calL1GasCost = async () => ethers.BigNumber.from(await wallet.provider.estimateL1GasCost(tx))
   const fetchEthPrice = async () => await fetchPrice('0x4200000000000000000000000000000000000006')
-  
 
-  const [l1gasCost, [gasPrice, l2gasCost], ethPrice] = await Promise.all([calL1GasCost, calL2GasCost, fetchEthPrice])
-  
+  const [l1gasCost, [pWeiValue, l2gasCost], ethPrice] = await Promise.all([calL1GasCost(), calL2GasCost(), fetchEthPrice()])
+
   const totalWei = l2gasCost.add(l1gasCost)
   const totalEth = ethers.utils.formatEther(totalWei);
 
   const totalUsd = ethPrice * totalEth
   
-  return [gasPrice, totalUsd]
+  return [pWeiValue, totalUsd]
 
 }
 
@@ -73,28 +78,24 @@ const main = async () => {
   
     const txReq = await vaultContract.populateTransaction.claimBounty(i, TO_ADDRESS)
     const tx = await wallet.populateTransaction(txReq)
-    const [gasPrice, totalCost] = await calculateGasFeeUsd(wallet, tx)
-
-    console.log('l2gas gwei', ethers.utils.formatUnits(gasPrice, 'gwei'))
-    console.log(bountyUsd, totalCost)
+    const [pWeiValue, totalCost] = await calculateGasFeeUsd(wallet, tx)
   
     if (!(bountyUsd - totalCost >= minReward)) {
       // console.log(88)
       continue
     }
-  
-    // TODO
-    // console.log(totalCost, bountyUsd, 123123)
-  
-    process.exit()
+
+    console.log(i, bountyUsd, totalCost)
+
+    if (argv.exec) {
+      tx.maxPriorityFeePerGas = pWeiValue
+
+      const txResponse = await wallet.sendTransaction(tx);
+      const receipt = await txResponse.wait();
+      console.log(receipt)
+    }
+
   }
-
-  // let realTx = await vaultContract.claimBounty(0, TO_ADDRESS)
-  // realTx.gasPrice = gasPrice
-
-  // const r = await realTx.wait()
-
-  
 }
 
 
